49eea1afeb68ae90b0d600c4ed64a6b4d5bd4fab
Amanda Wixted <amanda@jumprope.com>
2019-03-07 12:17:25 -0500

    manually creating a ViewController and displaying it

--- TextureCollectionView/ViewController.swift
+++ TextureCollectionView/ViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import AsyncDisplayKit
 
-class ViewController: UIViewController {
+class ViewController: ASViewController<ASCollectionNode> {
 
+    let flowLayout: UICollectionViewFlowLayout
+    let collectionNode: ASCollectionNode
+
+    init() {
+        flowLayout = UICollectionViewFlowLayout()
+        collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
+        collectionNode.backgroundColor = .green
+        super.init(node: collectionNode)
+
+        flowLayout.minimumInteritemSpacing = 1
+        flowLayout.minimumLineSpacing = 1
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
