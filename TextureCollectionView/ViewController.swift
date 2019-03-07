0d354b20c6185b5e9a882193e007b1b5256000b7
Amanda Wixted <amanda@jumprope.com>
2019-03-07 12:45:00 -0500

    have VideoViewControllers in our cell nodes

--- TextureCollectionView/ViewController.swift
+++ TextureCollectionView/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 import AsyncDisplayKit
 
-class ViewController: ASViewController<ASCollectionNode> {
+class VideoViewController: ASViewController<ASVideoNode> {
+
+    init(url: String) {
+        let videoNode = ASVideoNode()
+        videoNode.backgroundColor = .blue
+        super.init(node: videoNode)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class ViewController: ASViewController<ASCollectionNode>, ASCollectionDelegate, ASCollectionDataSource {
 
     let flowLayout: UICollectionViewFlowLayout
     let collectionNode: ASCollectionNode
+
+    let urls = ["hey"]
 
     init() {
         flowLayout = UICollectionViewFlowLayout()
@@ -30,9 +45,33 @@ class ViewController: ASViewController<ASCollectionNode> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionNode.dataSource = self
+        collectionNode.delegate = self
     }
 
+    func collectionNode(_ collectionNode: ASCollectionNode, nodeForItemAt indexPath: IndexPath) -> ASCellNode {
+        guard (0..<urls.count).contains(indexPath.row) else { return ASCellNode() }
+
+        let url = urls[indexPath.row]
+
+        let node = ASCellNode(viewControllerBlock: { () -> UIViewController in
+            return VideoViewController(url: url)
+        }, didLoad: { (displayNode) in
+            print("didLoad \(displayNode)")
+        })
+
+        node.style.preferredSize = collectionNode.bounds.size
+        return node
+    }
+
+    func numberOfSections(in collectionNode: ASCollectionNode) -> Int {
+        return 1
+    }
+
+    func collectionNode(_ collectionNode: ASCollectionNode, numberOfItemsInSection section: Int) -> Int {
+        return urls.count
+    }
 
 }
 
